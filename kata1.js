71ef07b78c0ba04d9d8e807e437b2537adc60fc1
MigoniDowsey <mdowsey@icstars.org>
2025-10-13 18:39:00 -0500

    md

---
+++ kata1.js
@@ -0,0 +1,5 @@
+// write a program that logs the odd numbers with 10
+
+for (let odd=0; odd<10;oddnumbers+2){ 
+    console.log(oddnumbers);
+}
---
+++ kata2.js
@@ -0,0 +1,10 @@
+for(let Fizz = 3; Fizz<=20; Fizz++)
+if(Fizz %3 === 0 && Fizz %5 === 0) {
+    console.log ("Fizzbuzz")
+} else if (Fizz % 3 === 0) {
+    console.log("Fizz")
+} else if (Fizz % 5 === 0) {
+    console.log("Buzz")
+} else {
+    console.log(Fizz                                                                                            )
+}
