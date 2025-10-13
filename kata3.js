494235b2e5bb596af78762108acd65b6c6fb7faf
Bonny <bmakaniankhondo@icstars.org>
2025-10-13 18:55:20 -0500

    I love using the CLI to commit feeling like a hacker :)

--- kata3.js
+++ kata3.js
@@ -3,3 +3,4 @@ let num = 10;
 for (let i = 1; i <= 5; i++) {
   console.log(`${num} x ${i} = ${num * i}`);
 }
+//this a comment i forgot to push earlier. This program makes a sweet multiplication table
