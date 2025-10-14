58fc7c994afa1dd8fa53c8d783df64770761cf07
pdarling-cloud <pdarling@icstars.org>
2025-10-14 11:23:22 -0500

    10/14/2025 Deliverables

---
+++ kata3.js
@@ -0,0 +1,7 @@
+// Write a multgiplication table from 1 to 5.
+
+let number = 1;
+console.log(`Multiplication Table for ${number}`);
+for (let i = 1; i <= 5; i++) {
+    console.log(`${number} * ${i} = ${number * i}`);
+}
