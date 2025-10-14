bd59e4223e519a1023b58718f311dd90be428dfb
MigoniDowsey <mdowsey@icstars.org>
2025-10-14 13:00:48 -0500

    Kata deliverable

--- kata2.js
+++ kata2.js
@@ -1,7 +1,7 @@
 for(let Fizz = 3; Fizz<=20; Fizz++)
-if(Fizz %3 === 0 && Fizz %5 === 0) {
+if(Fizz %3 == 0 && Fizz %5 == 0) {
     console.log ("Fizzbuzz")
-} else if (Fizz % 3 === 0) {
+} else if (Fizz %3 === 0) {
     console.log("Fizz")
 } else if (Fizz % 5 === 0) {
     console.log("Buzz")
--- kata3.js
+++ kata3.js
@@ -1,6 +1,6 @@
-let num=8
+let numbers=5
 
 for (let i = 1; i<=5; i++) {
-    console.log (`${num} x ${i} = ${num *i}`)
+    console.log (`${numbers} x ${i} = ${numbers *i}`)
 }
 //I am writing a program that prints a multiplication table from 1 to 5.
